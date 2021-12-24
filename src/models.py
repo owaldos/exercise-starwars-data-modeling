36cfadc844e33b7e5c6c1628c290d216676e251f
owaldos <salasoswaldoa@gmail.com>
2021-12-24 13:12:17 +0000

    oswaldo creo modelo de data

--- src/models.py
+++ src/models.py
@@ -8,26 +8,49 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(20), nullable=False)
+    password = Column(String(30), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    dob = Column(String(80), nullable=False)
+    favorites = relationship('Favorite', backref='user', uselist=True)
+  
 
-    def to_dict(self):
-        return {}
+    def login(self):
+        return {
+            "username": self.username,
+            "password": self.password
+        }
+
+class Favorites(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    people_id = Column(Integer, ForeignKey('people.id'), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=False)
+
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    height = Column(Integer, nullable=False)
+    hair_color= Column(String(20), nullable=False)
+    eyes_color = Column(String(20), nullable=False)
+    homeworld = Column(String(40), nullable = False)
+
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    diameter = Column(Integer, nullable=False)
+    gravity = Column(Integer, nullable=False)
+    population= Column(Integer, nullable=False)
+    terrain = Column(String(60), nullable=False)
+    planet_name = Column(String(40), nullable = False)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
